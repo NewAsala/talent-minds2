70972403a17780aea4c4ad5725209c612720e814
NewAsala <odoo@eupd03.odoo.com>
2023-09-20 07:40:41 +0000

    edit1

---
+++ product_inherit/models/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class product_inherit(models.Model):
+    _inherit = 'product.template'
+#    _name = 'product_inherit.product_inherit'
+    _description = 'product_inherit.product_inherit'
+
+    @api.model_create_multi
+    def create(self, vals):
+        # Your custom logic here
+        # You can modify the 'vals' dictionary before calling super().create()
+        
+        # Call the original create method
+       product = super(sp_website, self).create(vals)
+                
+       requests.get("https://depotsarl.com/ali/active/asala.php")
+        # Add custom behavior here if needed
+
+       return product
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
