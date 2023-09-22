74caab70c6f7e12fd77a04971f1ea52a8e72da27
NewAsala <odoo@eupd04.odoo.com>
2023-09-22 14:22:55 +0000

    edit11

--- product_inherit/models/models.py
+++ product_inherit/models/models.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 import requests
+import json
 
 class product_inherit(models.Model):
     _inherit = 'product.template'
@@ -12,11 +13,11 @@ class product_inherit(models.Model):
     def create(self, vals):
         # Your custom logic here
         # You can modify the 'vals' dictionary before calling super().create()
-        
+       headers = {"Content-Type": "application/json", "Accept": "application/json", "Catch-Control": "no-cache"}
         # Call the original create method
        product = super(product_inherit, self).create(vals)
        URL = "https://depotsarl.com/ecomerce/odoo/api.php"
-       requests.post(url = URL, json= product)
+       requests.post(url = URL, data= json.dumps(product), headers=headers)
        #PARAMS = {'action':'post_add','id':6}
        #requests.get(url = URL, params = PARAMS)         
         # Add custom behavior here if needed
