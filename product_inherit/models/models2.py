487cc29fe92bb1e46938ff960e15776f2c7c5fff
NewAsala <odoo@eupd04.odoo.com>
2023-09-25 13:14:31 +0000

    edit15

---
+++ product_inherit/models/models2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+import requests
+import json
+
+class product_product_inherit(models.Model):
+    _inherit = 'product.product'
+    _description = 'product_product_inherit'
+
+    @api.model_create_multi
+    def unlink(self):
+        product = super(product_inherit, self).unlink()
+        URL = "https://depotsarl.com/ecomerce/odoo/api.php"
+        PARAMS = {'action':'post_delete','id':self.id}
+        requests.get(url = URL, params = PARAMS)
+        return product
